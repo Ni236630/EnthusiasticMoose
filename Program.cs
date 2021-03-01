f0b297c801c214feafaf42341e88bcdf02494ba2
Ni236630 <nicholas.douso@gmail.com>
2021-03-01 10:29:04 -0600

    first commit

--- Program.cs
+++ Program.cs
@@ -1,36 +1,40 @@
 ﻿using System;
 
+Main();
 
-Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
-Console.WriteLine("--------------------------------------------");
-Console.WriteLine();
- 
- //moose speaks
-MooseSays("Hi, I'm Enthusiastic");
-MooseSays("I really am enthusiastic");
+void Main()
+{
+  Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+  Console.WriteLine("--------------------------------------------");
+  Console.WriteLine();
 
- //moose with question
-CanadaQuestion();
-EnthusiasticQuestion();
-LoveCSharpQuestion();
-SecretQuestion();
+  //moose speaks
+  MooseSays("Hi, I'm Enthusiastic");
+  MooseSays("I really am enthusiastic");
+
+  //moose with question
+  CanadaQuestion();
+  EnthusiasticQuestion();
+  LoveCSharpQuestion();
+  SecretQuestion();
+}
 
 void CanadaQuestion()
 {
-    bool isTrue = MooseAsks("Is Canada Real?");
-    if (isTrue)
-    {
-     MooseSays("Really? It seems very unlikely.");
-    }
-    else
-    {
-        MooseSays("I KNEW IT!!!");
-    } 
+  bool isTrue = MooseAsks("Is Canada Real?");
+  if (isTrue)
+  {
+    MooseSays("Really? It seems very unlikely.");
+  }
+  else
+  {
+    MooseSays("I KNEW IT!!!");
+  }
 }
 
 void MooseSays(string message)
 {
-     Console.WriteLine($@"
+  Console.WriteLine($@"
                                        _.--^^^--,
                                     .'          `\
   .-^^^^^^-.                      .'              |
@@ -59,63 +63,63 @@ void MooseSays(string message)
                        `^^` `^^^`
     ");
 
-} 
+}
 
 bool MooseAsks(string question)
 {
-    Console.Write($"{question} (Y/N): ");
-    string answer = Console.ReadLine().ToLower();
-    
-    while (answer != "y" && answer != "n")
-    {
-        Console.WriteLine($"{question} (Y/N): ");
-        answer = Console.ReadLine().ToLower();
-    }
-    
-    if (answer == "y")
-    {
-        return true;
-    }
-    else
-    {
-        return false;    
-    }
+  Console.Write($"{question} (Y/N): ");
+  string answer = Console.ReadLine().ToLower();
+
+  while (answer != "y" && answer != "n")
+  {
+    Console.WriteLine($"{question} (Y/N): ");
+    answer = Console.ReadLine().ToLower();
+  }
+
+  if (answer == "y")
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 void EnthusiasticQuestion()
 {
-    bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
-    if (isEnthusiastic)
-    {
-        MooseSays("Yay!");
-    }
-    else
-    {
-        MooseSays("You should try it!");
-    }
+  bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
+  if (isEnthusiastic)
+  {
+    MooseSays("Yay!");
+  }
+  else
+  {
+    MooseSays("You should try it!");
+  }
 }
 
 void LoveCSharpQuestion()
 {
-    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
-    if (doesLoveCSharp)
-    {
-        MooseSays("Good job sucking up to your instructor!");
-    }
-    else
-    {
-        MooseSays("You will...oh, yes, you will...");
-    }
+  bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+  if (doesLoveCSharp)
+  {
+    MooseSays("Good job sucking up to your instructor!");
+  }
+  else
+  {
+    MooseSays("You will...oh, yes, you will...");
+  }
 }
 
 void SecretQuestion()
 {
-    bool wantsSecret = MooseAsks("Do you want to know a secret?");
-    if (wantsSecret)
-    {
-        MooseSays("ME TOO!!!! I love secrets...tell me one!");
-    }
-    else
-    {
-        MooseSays("Oh, no...secrets are the best, I love to share them!");
-    }
+  bool wantsSecret = MooseAsks("Do you want to know a secret?");
+  if (wantsSecret)
+  {
+    MooseSays("ME TOO!!!! I love secrets...tell me one!");
+  }
+  else
+  {
+    MooseSays("Oh, no...secrets are the best, I love to share them!");
+  }
 }
